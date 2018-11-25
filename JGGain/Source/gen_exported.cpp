6cdd8a3ecaa91478b884fc676cec8532e7c3d6ac
Sam Tarakajian <starakajian@gmail.com>
2018-11-25 16:14:20 -0500

    Adding readme for building JUCE plugins

---
+++ JGGain/Source/gen_exported.cpp
@@ -0,0 +1,247 @@
+#include "gen_exported.h"
+
+namespace gen_exported {
+
+/*******************************************************************************************************************
+Cycling '74 License for Max-Generated Code for Export
+Copyright (c) 2016 Cycling '74
+The code that Max generates automatically and that end users are capable of exporting and using, and any
+  associated documentation files (the “Software”) is a work of authorship for which Cycling '74 is the author
+  and owner for copyright purposes.  A license is hereby granted, free of charge, to any person obtaining a
+  copy of the Software (“Licensee”) to use, copy, modify, merge, publish, and distribute copies of the Software,
+  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The Software is licensed to Licensee only for non-commercial use. Users who wish to make commercial use of the
+  Software must contact the copyright owner to determine if a license for commercial use is available, and the
+  terms and conditions for same, which may include fees or royalties. For commercial use, please send inquiries
+  to licensing (at) cycling74.com.  The determination of whether a use is commercial use or non-commercial use is based
+  upon the use, not the user. The Software may be used by individuals, institutions, governments, corporations, or
+  other business whether for-profit or non-profit so long as the use itself is not a commercialization of the
+  materials or a use that generates or is intended to generate income, revenue, sales or profit.
+The above copyright notice and this license shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+  THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+*******************************************************************************************************************/
+
+// global noise generator
+Noise noise;
+static const int GENLIB_LOOPCOUNT_BAIL = 100000;
+
+
+// The State struct contains all the state and procedures for the gendsp kernel
+typedef struct State {
+	CommonState __commonstate;
+	int __exception;
+	int vectorsize;
+	t_sample samplerate;
+	t_sample m_gain_1;
+	// re-initialize all member variables;
+	inline void reset(t_param __sr, int __vs) {
+		__exception = 0;
+		vectorsize = __vs;
+		samplerate = __sr;
+		m_gain_1 = 0;
+		genlib_reset_complete(this);
+		
+	};
+	// the signal processing routine;
+	inline int perform(t_sample ** __ins, t_sample ** __outs, int __n) {
+		vectorsize = __n;
+		const t_sample * __in1 = __ins[0];
+		const t_sample * __in2 = __ins[1];
+		t_sample * __out1 = __outs[0];
+		t_sample * __out2 = __outs[1];
+		if (__exception) {
+			return __exception;
+			
+		} else if (( (__in1 == 0) || (__in2 == 0) || (__out1 == 0) || (__out2 == 0) )) {
+			__exception = GENLIB_ERR_NULL_BUFFER;
+			return __exception;
+			
+		};
+		t_sample dbtoa_3 = dbtoa(m_gain_1);
+		// the main sample loop;
+		while ((__n--)) {
+			const t_sample in1 = (*(__in1++));
+			const t_sample in2 = (*(__in2++));
+			t_sample mul_2 = (in1 * dbtoa_3);
+			t_sample out1 = mul_2;
+			t_sample mul_4 = (in2 * dbtoa_3);
+			t_sample out2 = mul_4;
+			// assign results to output buffer;
+			(*(__out1++)) = out1;
+			(*(__out2++)) = out2;
+			
+		};
+		return __exception;
+		
+	};
+	inline void set_gain(t_param _value) {
+		m_gain_1 = (_value < -70 ? -70 : (_value > 12 ? 12 : _value));
+	};
+	
+} State;
+
+
+///
+///	Configuration for the genlib API
+///
+
+/// Number of signal inputs and outputs
+
+int gen_kernel_numins = 2;
+int gen_kernel_numouts = 2;
+
+int num_inputs() { return gen_kernel_numins; }
+int num_outputs() { return gen_kernel_numouts; }
+int num_params() { return 1; }
+
+/// Assistive lables for the signal inputs and outputs
+
+const char *gen_kernel_innames[] = { "in1", "in2" };
+const char *gen_kernel_outnames[] = { "out1", "out2" };
+
+/// Invoke the signal process of a State object
+
+int perform(CommonState *cself, t_sample **ins, long numins, t_sample **outs, long numouts, long n) {
+	State* self = (State *)cself;
+	return self->perform(ins, outs, n);
+}
+
+/// Reset all parameters and stateful operators of a State object
+
+void reset(CommonState *cself) {
+	State* self = (State *)cself;
+	self->reset(cself->sr, cself->vs);
+}
+
+/// Set a parameter of a State object
+
+void setparameter(CommonState *cself, long index, t_param value, void *ref) {
+	State *self = (State *)cself;
+	switch (index) {
+		case 0: self->set_gain(value); break;
+		
+		default: break;
+	}
+}
+
+/// Get the value of a parameter of a State object
+
+void getparameter(CommonState *cself, long index, t_param *value) {
+	State *self = (State *)cself;
+	switch (index) {
+		case 0: *value = self->m_gain_1; break;
+		
+		default: break;
+	}
+}
+
+/// Get the name of a parameter of a State object
+
+const char *getparametername(CommonState *cself, long index) {
+	if (index >= 0 && index < cself->numparams) {
+		return cself->params[index].name;
+	}
+	return 0;
+}
+
+/// Get the minimum value of a parameter of a State object
+
+t_param getparametermin(CommonState *cself, long index) {
+	if (index >= 0 && index < cself->numparams) {
+		return cself->params[index].outputmin;
+	}
+	return 0;
+}
+
+/// Get the maximum value of a parameter of a State object
+
+t_param getparametermax(CommonState *cself, long index) {
+	if (index >= 0 && index < cself->numparams) {
+		return cself->params[index].outputmax;
+	}
+	return 0;
+}
+
+/// Get parameter of a State object has a minimum and maximum value
+
+char getparameterhasminmax(CommonState *cself, long index) {
+	if (index >= 0 && index < cself->numparams) {
+		return cself->params[index].hasminmax;
+	}
+	return 0;
+}
+
+/// Get the units of a parameter of a State object
+
+const char *getparameterunits(CommonState *cself, long index) {
+	if (index >= 0 && index < cself->numparams) {
+		return cself->params[index].units;
+	}
+	return 0;
+}
+
+/// Get the size of the state of all parameters of a State object
+
+size_t getstatesize(CommonState *cself) {
+	return genlib_getstatesize(cself, &getparameter);
+}
+
+/// Get the state of all parameters of a State object
+
+short getstate(CommonState *cself, char *state) {
+	return genlib_getstate(cself, state, &getparameter);
+}
+
+/// set the state of all parameters of a State object
+
+short setstate(CommonState *cself, const char *state) {
+	return genlib_setstate(cself, state, &setparameter);
+}
+
+/// Allocate and configure a new State object and it's internal CommonState:
+
+void *create(t_param sr, long vs) {
+	State *self = new State;
+	self->reset(sr, vs);
+	ParamInfo *pi;
+	self->__commonstate.inputnames = gen_kernel_innames;
+	self->__commonstate.outputnames = gen_kernel_outnames;
+	self->__commonstate.numins = gen_kernel_numins;
+	self->__commonstate.numouts = gen_kernel_numouts;
+	self->__commonstate.sr = sr;
+	self->__commonstate.vs = vs;
+	self->__commonstate.params = (ParamInfo *)genlib_sysmem_newptr(1 * sizeof(ParamInfo));
+	self->__commonstate.numparams = 1;
+	// initialize parameter 0 ("m_gain_1")
+	pi = self->__commonstate.params + 0;
+	pi->name = "gain";
+	pi->paramtype = GENLIB_PARAMTYPE_FLOAT;
+	pi->defaultvalue = self->m_gain_1;
+	pi->defaultref = 0;
+	pi->hasinputminmax = false;
+	pi->inputmin = 0;
+	pi->inputmax = 1;
+	pi->hasminmax = true;
+	pi->outputmin = -70;
+	pi->outputmax = 12;
+	pi->exp = 0;
+	pi->units = "";		// no units defined
+	
+	return self;
+}
+
+/// Release all resources and memory used by a State object:
+
+void destroy(CommonState *cself) {
+	State *self = (State *)cself;
+	genlib_sysmem_freeptr(cself->params);
+		
+	delete self;
+}
+
+
+} // gen_exported::
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
+[001-project-settings]: 	img/001-project-settings.png
+[002-project-name]: 		img/002-project-name.png
+[003-plugin-code]:	 		img/003-plugin-code.png
+[004-export-path]:	 		img/004-export-path.png
+[005-juce-build]:	 		img/005-juce-build.png
+[006-xcode-build]:	 		img/006-xcode-build.png
+
 # gen-workshop
 Materials for the gen~ workshop and presentation at GRAME
+
+## Building a VST/AU using gen~
+In order to build a VST/AU using gen~, a little bit of finesse is required. We make use of the Projucer in order to create an XCode project that builds the appropriate plugin.
+
+1. Download the Projucer (see https://juce.com).
+2. Copy the folder named JUCEPluginTemplate. Copy this folder any time you would like to build a new VST/AU using gen~. You may rename this folder.
+3. Open the file C74ProjucerGenPlugin.jucer.
+4. Navigate to the Project Settings.
+
+![Project Settings Image][001-project-settings]
+
+5. Change the Project Name to be whatever you like. This will be the name of the plugin within any host application.
+
+![Project Name Image][002-project-name]
+
+6. Change the Plugin Code to be anything unique. You may also change the Plugin Manufacturer Code and Plugin Manufacturer if you like. The only rule is, no two plugins may have the same Plugin Manufacturer Code and Plugin Code. The other only rule is, the Plugin Manufacturer Code and Plugin Code must contain at least one upper-case letter. Apple's own internal plugins may violate this rule, apparently.
+
+![Plugin Code Image][003-plugin-code]
+
+7. Make whatever kind of gen~ patcher your heart desires, according to normal gen~ usage.
+
+8. Export the gen code. Don't change the _exportname_ attribute. Be sure to export it to the Source folder of your new JUCE project. This where the JUCE project expects it to be (the header search path is configured to look for headers here).
+
+![Export Path Image][004-export-path]
+
+9. Build the XCode project in JUCE. This should automatically open the XCode project.
+
+![Build JUCE Image][005-juce-build]
+
+10. Build the XCode project. Without the VST3 SDK installed, you'll need to build an Audio Unit. This will build the actual component.
+
+![Build XCode Image][006-xcode-build]
+
+11. This will install the component in `~/Library/Audio/Plug-Ins/Components`.
+
+12. If you find that the component is still not registered (you run `auval -a` from the command line and don't see your component), then there is one thing you can try (I don't understand why this is a solution). Return to JUCE, change the Plugin Code to anything, rebuild the XCode project and rebuild the plugin. Run `auval -a` again and you should see the plugin under the new name. You can then change the Plugin Code back. I don't know why this is necessary.
+
+12. That should be everything you need to do to build and install your plugin. From this point on, if you need to change your plugin, you need only export the code from Max and rebuild the plugin in XCode. You need not open JUCE again, unless you want to change the name of the plugin or some other configuration.
